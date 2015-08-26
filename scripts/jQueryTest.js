df911a68cfdbd83081ef30b8787d95774401fc06
javier <silveriofunk@gmail.com>
2015-08-27 00:15:32 +0100

    adds button exercise

--- scripts/jQueryTest.js
+++ scripts/jQueryTest.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
-    $("#red" ).fadeOut(2000);
+    $("#square1" ).fadeOut(2000);
 });
 
 $(document).ready(function() {
-    $("#purple" ).fadeOut(1000);
+    $("#square2" ).fadeOut(1000);
+});
+$(document).ready(function() {
+	$('button').mouseenter(function(){
+		$('button').fadeTo('fast', 1);
+	});
+	$('button').mouseleave(function(){
+		$('button').fadeTo('slow', 0.8);
+	});
 });
